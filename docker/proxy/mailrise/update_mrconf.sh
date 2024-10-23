507ab36f8c8981c24a5d5c58a294e526d39886ce
adf1969 <andrew@avcorp.biz>
2024-10-22 22:04:10 -0500

    Create update_mrconf.sh to process the maijlrise.conf file and fix any env vars in it. Update docker-compose to add code for enabling debuging for mailrise, if we want it.

---
+++ docker/proxy/mailrise/update_mrconf.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Define the paths
+MAILRISE_CONF_TEMPLATE="/opt/divtools/docker/proxy/mailrise/mailrise.conf"
+ENV_FILE="/opt/divtools/docker/proxy/.env"
+MAILRISE_CONF_OUTPUT="/opt/mailrise/mailrise.conf"
+
+# Load environment variables from the .env file
+export $(grep -v '^#' "$ENV_FILE" | xargs)
+
+# Use envsubst to replace variables in the template and save the result to the output file
+envsubst < "$MAILRISE_CONF_TEMPLATE"> "$MAILRISE_CONF_OUTPUT"
+
+# Check if the substitution was successful
+if [ $? -eq 0 ]; then
+    echo "mailrise.conf has been successfully updated and saved to $MAILRISE_CONF_OUTPUT."
+else
+    echo "Error: Failed to process the mailrise.conf template."
+    exit 1
+fi
